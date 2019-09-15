40435723c3a3b45cce95f044d3cd3a64ee70d548
M.D. van Es <es@mdworld.nl>
2019-09-15 20:44:12 +0200

    Use elm-webpack-loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -472,6 +472,12 @@ module.exports = function(webpackEnv) {
                 'sass-loader'
               ),
             },
+            // loader for elm files
+            {
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loader: require.resolve('elm-webpack-loader')
+            },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
@@ -483,7 +489,7 @@ module.exports = function(webpackEnv) {
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
               // by webpacks internal loaders.
-              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/, /\.elm$/],
               options: {
                 name: 'static/media/[name].[hash:8].[ext]',
               },
