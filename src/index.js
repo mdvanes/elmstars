853ae552d0ef6af2a43e6cb07c328fedbf0f59c9
M.D. van Es <es@mdworld.nl>
2019-09-16 20:18:52 +0200

    Build script for standalone React component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ElmStars from './ElmStars';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import ElmStars from './ElmStars';
+//
+// ReactDOM.render(<ElmStars />, document.getElementById('root'));
 
-ReactDOM.render(<ElmStars />, document.getElementById('root'));
+import React from 'react';
+class Fancy extends React.Component {
+  render() {
+    return (
+      <div>This is so Fancy!</div>
+    );
+  }
+}
+export default Fancy;
