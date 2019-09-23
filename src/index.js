536eb47504ba595254f6924013fff05f127a8080
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2019-09-23 14:33:51 +0200

    Restore loading directly into CRA

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import ElmStars from './ElmStars';
-//
-// ReactDOM.render(<ElmStars />, document.getElementById('root'));
-
 import React from 'react';
-class Fancy extends React.Component {
-  render() {
-    return (
-      <div>This is so Fancy!</div>
-    );
-  }
-}
-export default Fancy;
+import ReactDOM from 'react-dom';
+import './index.css';
+import ElmStars from './ElmStars';
+
+ReactDOM.render(<ElmStars />, document.getElementById('root'));
